a126eec942425f72aa4a21575f4127786c6aa4cd
kureharyosuke <ohhayo.com@gmail.com>
2020-10-01 00:06:35 +0900

    #3. React-Router-Dom Install

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FcFlashOn } from "react-icons/fc";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
   return (
     <>
-      <div className="navbar"></div>
+      <div className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            TOLAB
+            <FcFlashOn className="navbar-icon" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
