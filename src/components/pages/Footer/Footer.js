39b1d9590225aeaf8bc37546a74cd02497ebf9da
kureharyosuke <ohhayo.com@gmail.com>
2020-10-11 00:38:23 +0900

    #9. Footer Logo Change

--- src/components/pages/Footer/Footer.js
+++ src/components/pages/Footer/Footer.js
@@ -76,7 +76,7 @@ function Footer() {
               <FcFlashOn className="navbar-icon" />
             </Link>
           </div>
-          <small className="website-rights">LAVISH © 2020</small>
+          <small className="website-rights">PARTNER © 2020</small>
           <div className="social-icons">
             <Link
               className="social-icon-link"
